acd1f5ccd05fa6e11afba8495ec29af35193fc9f
Arini Nur Azizah <155711234+sun7ren@users.noreply.github.com>
2025-07-22 08:21:14 +0700

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import { Playfair_700Bold } from "@expo-google-fonts/playfair";
 import { useFonts } from "expo-font";
 import React, { useState } from "react";
-import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+import { styles } from './styles';
 
 interface TaskItem {
   text: string;
@@ -108,75 +110,3 @@ const Index = () => {
 };
 
 export default Index;
-export { TaskItem };
-
-const styles = StyleSheet.create({
-  playfair: {
-    fontSize: 30,
-    fontFamily: "Playfair_700Bold",
-  },
-  input: {
-    padding: 10,
-    borderColor: 'darkgray',
-    borderWidth: 1,
-    marginVertical: 3,
-    marginLeft: 20,
-    marginRight: 10,
-    height: 40,
-    borderRadius: 10,
-  },
-  button: {
-    backgroundColor: 'rgba(162, 91, 148, 1)',
-    padding: 10,
-    marginVertical: 5,
-    borderRadius: 10,
-    marginRight: 20,
-  },
-  background: {
-    backgroundColor: 'rgba(255, 206, 244, 1)',
-    borderWidth: 1,
-    borderColor:'rgba(255, 183, 239, 1)',
-    borderRadius: 10,
-    padding: 10,
-    color: 'rgba(125, 74, 115, 1)',
-  },
-  task: {
-    fontSize: 18,
-    fontFamily: "Playfair_700Bold",
-  },
-  deleteButton: {
-    backgroundColor: 'rgba(162, 91, 148, 1)',
-    padding: 5,
-    borderRadius: 5,
-    marginTop: 5,
-    marginRight: 5
-  },
-  list: {
-    padding: 10,
-    marginVertical: 10,
-    marginHorizontal: 20,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: 'rgba(174, 128, 164, 1)',
-  },
-  image:{
-    marginTop: 30,
-    marginLeft: 95,
-    alignItems: 'center',
-    width: 200,
-    height: 200
-  },
-  checkButton: {
-    backgroundColor: 'rgba(162, 91, 148, 1)',
-    padding: 5,
-    borderRadius: 5,
-    marginTop: 5,
-  },
-  box: {
-    backgroundColor: 'rgba(212, 128, 194, 1)',
-    padding: 10,
-    margin: 5,
-    borderRadius: 10,
-    color: 'white'
-  }
-});
